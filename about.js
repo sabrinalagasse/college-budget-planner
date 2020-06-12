284d1fb720067e0f4c88fcf5c6967a7862953ab7
sabrinalagasse <lagassesabrina@gmail.com>
2020-06-12 13:55:53 -0700

    More changes idk

--- about.js
+++ about.js
@@ -1,7 +1,9 @@
 //access elements
 let aboutBtn = document.getElementById('openAbout');
 let aboutElement = document.getElementById('about');
+let closeIcon = document.querySelector('#about > i');
 
+closeIcon.onclick = toggleAbout;
 aboutBtn.onclick = toggleAbout;
 
 function toggleAbout() {
